861f1f61c278ddb127c41f4e9480ef5fe779516f
flufiorau <flufiorau@ukr.net>
2018-12-20 00:36:58 +0200

    events google calendar + firebase correctly shows

--- src/app/core/interfaces.ts
+++ src/app/core/interfaces.ts
@@ -12,6 +12,7 @@ export interface CalendarEventFromForm {
   eventDate: number;
   eventDescription?: string;
   eventMembers?: string;
+  googleCalendarEvent?: boolean;
 }
 
 export interface CalendarList {
